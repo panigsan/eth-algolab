59e22d10a14590a80ea865a4a84b9dc53cfb5c3f
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-09-27 17:59:19 +0200

    .

--- week2/search_snippets/search_snippets.cpp
+++ week2/search_snippets/search_snippets.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,46 +17,57 @@ void testcase(){
   }
 
 
-  set<pair<int,int>> poss;
+  vector<vector<int>> p;
+  set<pair<int,int>> data;
   for(int i=0;i<n;i++){
+    p.push_back(vector<int>());
     for(int j=0;j<m[i];j++){
       int pj; cin >> pj;
-      poss.insert({pj,i});
+      data.insert({pj,i});
+      p[i].push_back(pj);
     }
   }
 
-  vector<pair<int,int>> pos(poss.begin(),poss.end());
+  vector<int> pos;
+  vector<int> typs;
+  for(pair<int,int> d : data){
+    pos.push_back(d.first);
+    typs.push_back(d.second);
+  }
+
 
   vector<int> count(n,0);
   set<int> missing;
-
+  int l=0;
+  int r=0;
+  int best_d = numeric_limits<int>::max();
   for(int i=0;i<n;i++) missing.insert(i);
-  count[pos[0].second] = 1;
+  auto it = missing.find(typs[0]);
+  missing.erase(it);
+  count[typs[0]] = 1;
 
-  int l = 0;
-  int r = 0;
-  int best_d=numeric_limits<int>::max();
   while(l<tot_w && r<tot_w){
     if(missing.size()>0){
       r++;
-      int ty = pos[r].second;
+      if(r>=tot_w) break;
+      int ty = typs[r];
       count[ty]++;
+      auto it = missing.find(ty);
       if(missing.find(ty) != missing.end()){
-        missing.erase(ty);
+        missing.erase(it);
       }
     }else{
-      int d = pos[r].first - pos[l].first+1;
+      int d = pos[r] - pos[l]+1;
       if(d < best_d) {
         best_d = d;
       }
-      int ty = pos[l].second;
+      int ty = typs[l];
       count[ty]--;
       if(count[ty]==0){
         missing.insert(ty);
       }
       l++;
     }
-
   }
   cout << best_d << endl;
 }
