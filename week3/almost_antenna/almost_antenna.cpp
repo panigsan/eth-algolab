cc80423bd5e5b43ccb84d3f8f9b2f5968c2dca10
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-08 15:28:19 +0200

    refactoring

--- week3/almost_antenna/almost_antenna.cpp
+++ week3/almost_antenna/almost_antenna.cpp
@@ -27,8 +27,10 @@ int main(){
   while(true){
     std::cin >> n;
     if(n==0) break;
+
     points.clear();
     new_points.clear();
+
     for(int i=0;i<n;i++){
       long x,y;
       std::cin >> x >> y;
@@ -37,7 +39,7 @@ int main(){
 
     Min_circle mc(points.begin(),points.end(),true);
 
-    /*
+    // store in new_points all non-supporting points
     for(auto p : points){
       if(std::find(mc.support_points_begin(),mc.support_points_end(),p) ==
                    mc.support_points_end()){
@@ -45,40 +47,19 @@ int main(){
       }
     }
 
-    Min_circle new_mc(new_points.begin(),new_points.end(),true);
-    */
     K::FT best_squared_radius(std::numeric_limits<double>::max());
+
+    // try to leave out one support point at the time
     for(int i=0;i<mc.number_of_support_points();i++){
-      new_points.clear();
-      for(auto p : points){
-        if(std::find(mc.support_points_begin(),mc.support_points_end(),p) ==
-                     mc.support_points_end()){
-          new_points.push_back(p);
-        }
-      }
-      //Min_circle mc2(new_mc.support_points_begin(),new_mc.support_points_end());
       Min_circle mc2(new_points.begin(),new_points.end(),true);
       for(int j=0;j<mc.number_of_support_points();j++){
         if(i!=j) mc2.insert(mc.support_point(j));
       }
       Traits::Circle c2 = mc2.circle();
-      auto rad = c2.squared_radius();
-      //std::cout << ceil_to_double(CGAL::sqrt(rad)) << std::endl;
-      if(rad < best_squared_radius){
-        best_squared_radius = rad;
-      }
-    }
-    if(false && t==3){
-      int cnt = 0;
-      for(auto p : points){
-        if(mc.has_on_boundary(p)) ++cnt;
-      }
-      //std::cout << cnt << std::endl;
-      //std::cout << mc.number_of_support_points() << std::endl;
+      best_squared_radius = std::min(best_squared_radius,c2.squared_radius());
     }
 
     std::cout << ceil_to_double(CGAL::sqrt(best_squared_radius)) << std::endl;
 
-    t++;
   }
 }
