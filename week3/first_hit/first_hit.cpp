02021a10e33cd2d012803de9370abcfb5e7592e9
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-10 09:59:12 +0200

    first hit 100 points

--- week3/first_hit/first_hit.cpp
+++ week3/first_hit/first_hit.cpp
@@ -12,25 +12,6 @@
 typedef CGAL::Exact_predicates_exact_constructions_kernel K;
 typedef K::Point_2 P;
 typedef K::Segment_2 S;
-typedef std::tuple<long,long,long,long> Key;
-
-struct FourTuple {
-  long a,b,c,d;
-};
-
-struct MyHash{
-  std::size_t operator()(const Key& x) const noexcept {
-    using boost::hash_value;
-    using boost::hash_combine;
-    std::size_t seed = 0;
-    hash_combine(seed,hash_value(std::get<0>(x)));
-    hash_combine(seed,hash_value(std::get<1>(x)));
-    hash_combine(seed,hash_value(std::get<2>(x)));
-    hash_combine(seed,hash_value(std::get<3>(x)));
-
-    return seed;
-  }
-};
 
 double floor_to_double(const K::FT& x){
   double a = std::floor(CGAL::to_double(x));
@@ -42,55 +23,64 @@ double floor_to_double(const K::FT& x){
 int main(){
   std::ios_base::sync_with_stdio(false);
   std::cout << std::fixed << std::setprecision(0);
+
   int n;
-  std::unordered_set<std::tuple<long,long,long,long>,MyHash> visited;
-  //std::unordered_set<FourTuple,MyHash> visited;
+  std::vector<K::Segment_2> segs;
+
   while(true){
-    visited.clear();
     std::cin >> n;
     if(n==0) break;
+
     long x,y,a,b;
     std::cin >> x >> y >> a >> b;
     
     const P start(x,y);
     const P somewhere(a,b);
-    K::Ray_2 ray(start,somewhere);
+    const K::Ray_2 ray(start,somewhere);
 
-    bool found = false;
-    long mm = std::numeric_limits<long>::max();
+    K::FT best_sq_dist(std::numeric_limits<double>::max());
     P best;
+    // read and shuffle the input
+    segs.clear();
     for(int i=0;i<n;i++){
       long r,s,t,u;
       std::cin >> r >> s >> t >> u;
-      K::Segment_2 seg(P(r,s),P(t,u));
-      Key p(r,s,t,u);
-      if(visited.find(p) != visited.end()) continue;
-      visited.insert(p);
+      segs.push_back(K::Segment_2(P(r,s),P(t,u)));
+    }
+    std::random_shuffle(segs.begin(),segs.end());
+
+    for(const K::Segment_2 seg : segs){
+      // early stop or skip
+      if(best_sq_dist<=K::FT(1)) break;
+      if(best_sq_dist< CGAL::squared_distance(start,seg)) continue;
+
       if(CGAL::do_intersect(ray,seg)){
         auto o = CGAL::intersection(ray,seg);
         if(const P* op = boost::get<P>(&*o)){
-          if(!found || CGAL::has_smaller_distance_to_point(start,*op,best)){
+          K::FT d(CGAL::squared_distance(start,*op));
+          if(d < best_sq_dist){
             best = *op;
-            found = true;
+            best_sq_dist = d;
           }
-        }else if(const S* os = boost::get<S>(&*o)){
+        }else if( const S* os = boost::get<S>(&*o)){
           P source = os->source();
           P target = os->target();
-          if(!found || CGAL::has_smaller_distance_to_point(start,source,best)){
+          K::FT ds(CGAL::squared_distance(start,source));
+          if(ds < best_sq_dist){
             best = source;
-            found = true;
+            best_sq_dist = ds;
           }
-          if(!found || CGAL::has_smaller_distance_to_point(start,target,best)){
+          K::FT dt(CGAL::squared_distance(start,target));
+          if(dt < best_sq_dist){
             best = target;
-            found = true;
+            best_sq_dist = dt;
           }
         } else {
           throw std::runtime_error("should not happen");
         }
       }
     }
-the:
-    if(found) {
+    if(best_sq_dist != K::FT(std::numeric_limits<double>::max())) {
       std::cout << floor_to_double(best.x()) << " "
                 << floor_to_double(best.y()) << std::endl;
     }
