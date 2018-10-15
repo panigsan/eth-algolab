f92a8747e1c3bc5c2fab3d0eab21ab19e72bf814
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-15 23:32:49 +0200

    potw4 hiking maps 100

--- potw4_hiking_maps/potw4.cpp
+++ potw4_hiking_maps/potw4.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+#include <utility>
 #include <unordered_set>
 #include <set>
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
@@ -12,17 +14,38 @@ typedef K::Segment_2 S;
 typedef K::Triangle_2 T;
 using namespace std;
 
+#define trace(x) cout << #x << " = " << x << endl
+
+// if using a triangle
+inline bool point_in_trig(const T& t, const P& p){
+  auto b1 = t.bounded_side(p);
+  return b1 == CGAL::ON_BOUNDED_SIDE || b1==CGAL::ON_BOUNDARY;
+}
+
+// if using corners of a trianle
+inline bool point_in_trig(const tuple<P,P,P>& t, const P& p) {
+  bool a = !CGAL::left_turn(get<0>(t),get<1>(t),p);
+  bool b = !CGAL::left_turn(get<1>(t),get<2>(t),p);
+  // this line helps going from 80 to 100 points
+  if(a!=b) return false;
+  bool c = !CGAL::left_turn(get<2>(t),get<0>(t),p);
+
+  return b == c;
+}
+
 void testcase(){
   int m,n; cin >> m >> n;
 
   vector<P> points;
-  vector<T> trigs;
+  //vector<T> trigs;
+  vector<tuple<P,P,P>> trigs;
 
   for(int i=0;i<m;i++){
     int x, y; cin >> x >> y;
     points.push_back(P(x,y));
   }
 
+  // create the triangles
   for(int i=0;i<n;i++){
     vector<P> q;
     for(int j=0;j<6;j++){
@@ -37,107 +60,58 @@ void testcase(){
 
     auto o1 = CGAL::intersection(l1,l2);
     const P p1 = *(boost::get<P>(&*o1));
-    auto o2 = CGAL::intersection(l2,l3);
+    auto o2 = CGAL::intersection(l1,l3);
     const P p2 = *(boost::get<P>(&*o2));
-    auto o3 = CGAL::intersection(l3,l1);
+    auto o3 = CGAL::intersection(l2,l3);
     const P p3 = *(boost::get<P>(&*o3));
 
-    trigs.push_back(T(p1,p2,p3));
+    //trigs.push_back(T(p1,p2,p3));
+    trigs.push_back(make_tuple(p1,p2,p3));
   }
 
-  vector<vector<int>> cont_leg(n,vector<int>());
-  vector<vector<int>> pos(m-1,vector<int>());
+  // pos[i] contains the triangles which cover leg i
+  vector<queue<int>> pos(m-1,queue<int>());
 
+  // check where the legs belong
   for(int i=0;i<n;i++){
+    // keep the previous stored so that it is not computed twice
+    bool previous = point_in_trig(trigs[i],points[0]);
     for(int j=0;j<m-1;j++){
-      auto b1 = trigs[i].bounded_side(points[j]);
-      auto b2 = trigs[i].bounded_side(points[j+1]);
-      if((b1 == CGAL::ON_BOUNDED_SIDE || b1==CGAL::ON_BOUNDARY) &&
-         (b2 == CGAL::ON_BOUNDED_SIDE || b2==CGAL::ON_BOUNDARY)){
-        //cout << i << " has " << j << endl;
-        cont_leg[i].push_back(j);
-        pos[j].push_back(i);
-      }else {
-        //cout << i << " has not " << j << endl;
-      
+      bool next = point_in_trig(trigs[i],points[j+1]);
+      if(previous && next){
+        pos[j].push(i);
       }
+      previous = next;
     }
   }
-
-  /*
-  for(int i=0;i<n;i++){
-    cout << "i: " << i << ": " << cont_leg[i].size() << endl;
-    for(int x : cont_leg[i]) cout << x << " ";
-    cout << endl;
-  }*/
 
   int best = 10000;
 
-  /*
+  // search snippet
   set<pair<int,int>> idx;
   for(int i=0;i<m-1;i++){
-    idx.insert(make_pair(pos[i][0],i));
+    idx.insert(make_pair(pos[i].front(),i));
+    pos[i].pop();
   }
 
+  // idea:
+  // idx is an ordered set of tuples where the first entry
+  // contain an index of the triangle and the second contains
+  // the number of the leg
+  // we shift the window by removing the most-left pair,
+  // hence removing a leg, and finding the next idx for which 
+  // the removed leg is available (stored in the stack pos)
   while(1){
     pair<int,int> minp = *idx.begin();
     pair<int,int> maxp = *idx.rbegin();
-    int dist = maxp.first - minp.first;
+    int dist = maxp.first - minp.first+1;
     if(dist < best) best = dist;
     
-    auto it = upper_bound(pos[minp.second].begin(),pos[minp.second].end(),minp.first);
-    if(it==pos[minp.second].end())break;
+    if(pos[minp.second].size()==0) break;
+    int new_idx = pos[minp.second].front();
+    pos[minp.second].pop();
     idx.erase(idx.begin());
-    idx.insert(make_pair(*it,minp.second));
-
-  }
-  best++;
-  */
-
-  /*
-  
-  for(int b=0;b<n;b++){
-    for(int e=1;e<=n;e++){
-      if(e-b >= best) continue;
-      vector<bool> missing(m-1,true);
-      for(int i=b;i<e;i++){
-        for(int x : cont_leg[i]) missing[x] = false;
-      }
-      
-      bool good = true;
-      for(int x =0;x<m-1;x++){
-        if(missing[x]) good=false;
-      }
-      
-      if(good && e-b < best)
-        best = e-b;
-    }
-  }
-
-  */
-  unordered_set<int> missing;
-  for(int i=0;i<m-1;i++) missing.insert(i);
-  vector<int> cnt(m,0);
-  int b=0,e=0;
-  
-  while(e<n){
-    if(missing.size()>0){
-      for(int x : cont_leg[e]){
-        if(cnt[x]==0) missing.erase(missing.find(x));
-        cnt[x]++;
-      }
-      e++;
-    } else {
-      if(e-b < best){
-        best = e-b;
-        //cout << "new best " << b << " - " << e << endl;
-      }
-      for(int x : cont_leg[b]){
-        cnt[x]--;
-        if(cnt[x]==0) missing.insert(x);
-      }
-      b++;
-    }
+    idx.insert(make_pair(new_idx,minp.second));
   }
 
   cout << best << endl;
