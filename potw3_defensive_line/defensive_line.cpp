71fa2132519fb255db32c5c24a372acdd7ed0eb6
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-16 15:09:09 +0200

    defensive line - from dp[l][i] to dp[i][l]

--- potw3_defensive_line/defensive_line.cpp
+++ potw3_defensive_line/defensive_line.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int n,m,k;
 
 vector<int> v;
-// dp[l][i] = max attack when at line l and still have i attackers
+// dp[i][l] = max attack when having i attackers and beign at line l
 vector<vector<int>> dp;
 // bound[l] == -1 if starting at position l is not possible to attack a line
 // otherwise it will contain the index r at which the attacking line ends
@@ -23,7 +23,8 @@ void testcase(){
   v.resize(n+1,-1);
 
   dp.clear();
-  dp.resize(n+2,vector<int>(m+1,-1));
+  //dp.resize(n+2,vector<int>(m+1,-1));
+  dp.resize(m+1,vector<int>(n+2,-1));
   bound.clear();
   bound.resize(n+1,-1);
 
@@ -50,31 +51,31 @@ void testcase(){
   
   // starting from the end fill the dp
   l=n-1;
-  dp[n+1][0] = 0;
-  dp[n][0] = 0;
-  if(v[n]==k) dp[n][1] = 1;
+  dp[0][n+1] = 0;
+  dp[0][n] = 0;
+  if(v[n]==k) dp[1][n] = 1;
 
   while(l>=1){
-    dp[l][0] = 0;
+    dp[0][l] = 0;
     // check if possible to start attacking a line at position l
     int r = bound[l];
     if(r==-1){
       for(int i=1;i<=m;i++){
-        dp[l][i] = dp[l+1][i];
+        dp[i][l] = dp[i][l+1];
       }
     } else {
       for(int i=1;i<=m;i++){
-        if(dp[r+1][i-1] == -1) 
-          dp[l][i] = dp[l+1][i];
+        if(dp[i-1][r+1] == -1) 
+          dp[i][l] = dp[i][l+1];
         else
-          dp[l][i] = max(dp[l+1][i],(r-l+1)+dp[r+1][i-1]);
+          dp[i][l] = max(dp[i][l+1],(r-l+1)+dp[i-1][r+1]);
       }
     }
     l--;
   }
 
 
-  int res = dp[1][m];
+  int res = dp[m][1];
   if(res==-1) cout << "fail" << endl;
   else        cout << res << endl;
 }
