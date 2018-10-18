0a027054fa31a7fae22ec170502f83cfe9760427
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-18 22:29:43 +0200

    hst nicer version but still slow

--- week5/high_school_teams/hst.cpp
+++ week5/high_school_teams/hst.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 #include <cassert>
 #include <bitset>
+#include <cmath>
 
 using namespace std;
 
-#define trace(x) cout << #x << " = " << x << endl
+#define trace(x)  cout << #x << " = " << x << endl
 #define btrace(x) cout << #x << " = " << bitset<12>(x) << endl
+
 
 int n,k;
 vector<int> s;
+
 void testcase(){
   cin >> n >> k;
   s.clear();
@@ -18,49 +21,35 @@ void testcase(){
     cin >> s[i];
   }
 
-  int sol=0;
-  for(int m_out=0;m_out< 1<<n;++m_out){ // all subsets for K
-    int sk = 0;
-    for(int j=0; j<n;++j){
-      if(m_out & (1<<j)) ++sk;
-    }
-    if(sk>k) continue;
+  int sol = 0;
+  for(int m_in=0;m_in < (1<<n); ++m_in){
+    if(__builtin_popcount(m_in)<n-k) continue;
 
-    int goal=0;
-    for(int j=0;j<n;++j){
-      if(! (m_out & (1<<j))) goal += s[j];
+    int goal = 0;
+    for(int i=0;i<n;++i){
+      if(m_in & (1<<i)) goal += s[i];
     }
-    if(goal %2 == 1) continue; //if odd it is not possible to divide in two
-    goal = goal / 2;
-    
-    for(int m_red =0; m_red < 1<<n;++m_red){
-      if(m_out & m_red) continue;
-      int temp = 0;
-      for(int b=0;b<n;++b){
-        if(m_red & (1<<b)){
-          temp += s[b];
-        }
-      }
-      if(temp != goal) continue;
-      if(goal != 0 && temp==goal){
-        ++sol;
-        continue;
+    if(goal %2 == 1) continue;
+
+    int m_sub=0;
+    do {
+      int s1 = 0;
+      int s2 = 0;
+      for(int i=0;i<n;++i){
+        if(m_sub & (1<<i)) s1 += s[i];
+        else if( m_in & 1<<i) s2 += s[i];
       }
 
-      int temp2 = 0;
-      for(int b=0;b<n;++b){
-        if(!(m_red& (1<<b)) && !(m_out&(1<<b))){
-          temp2 += s[b];
-        }
-      }
-      if(temp2==goal) ++sol;
-    }
+      if(s1 == goal / 2 && s2 == goal/2) ++sol;
+    } while( (m_sub=(m_sub-m_in)&m_in) );
   }
+
   cout << sol << endl;
 }
 
 int main(){
   ios_base::sync_with_stdio(false);
+
   int T; cin >> T;
   while(T--) testcase();
 }
