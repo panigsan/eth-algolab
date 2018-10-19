7ed9b6b214e61114990572e4f83b175b03cd174a
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-10-19 15:23:43 +0200

    hst with DP, still slow but nice

--- week5/high_school_teams/hst.cpp
+++ week5/high_school_teams/hst.cpp
@@ -22,26 +22,21 @@ void testcase(){
   }
 
   int sol = 0;
-  for(int m_in=0;m_in < (1<<n); ++m_in){
-    if(__builtin_popcount(m_in)<n-k) continue;
-
-    int goal = 0;
-    for(int i=0;i<n;++i){
-      if(m_in & (1<<i)) goal += s[i];
+  vector<int> dp(1<<n);
+  for(int i=0;i<n;++i){
+    for(int j=0;j < (1<<i); ++j){
+      dp[j | (1<<i)] = s[i] + dp[j];
     }
-    if(goal %2 == 1) continue;
+  }
 
-    int m_sub=0;
-    do {
-      int s1 = 0;
-      int s2 = 0;
-      for(int i=0;i<n;++i){
-        if(m_sub & (1<<i)) s1 += s[i];
-        else if( m_in & 1<<i) s2 += s[i];
-      }
+  for(int m_in=0;m_in< (1<<n); ++m_in){
+    if(__builtin_popcount(m_in) < n-k) continue;
 
-      if(s1 == goal / 2 && s2 == goal/2) ++sol;
-    } while( (m_sub=(m_sub-m_in)&m_in) );
+    int m_red=0;
+    do{
+      if(dp[m_red] == dp[(~m_red) & m_in]) ++sol; 
+    } while ((m_red = (m_red - m_in) & m_in));
+  
   }
 
   cout << sol << endl;
