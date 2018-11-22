11e89a44963006fff43707fb441a29f6c6ac118c
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-11-22 23:41:40 +0100

    goldeneye 100

--- potw9_goldeneye/goldeneye.cpp
+++ potw9_goldeneye/goldeneye.cpp
@@ -3,6 +3,9 @@
 #include <CGAL/Triangulation_vertex_base_with_info_2.h>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include <boost/graph/connected_components.hpp>
+#include <boost/graph/incremental_components.hpp>
+#include <boost/pending/disjoint_sets.hpp>
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -16,22 +19,30 @@ typedef CGAL::Triangulation_face_base_2<K> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
 typedef CGAL::Delaunay_triangulation_2<K,Tds>  Triangulation;
 typedef Triangulation::Vertex_handle Vertex_handle;
+typedef std::size_t ll;
 
 using namespace boost;
-typedef std::size_t I;
-typedef adjacency_list < vecS, vecS, undirectedS,
-  no_property, property < edge_weight_t, I > > Graph;
-typedef graph_traits < Graph >::edge_descriptor Edge;
-typedef graph_traits < Graph >::out_edge_iterator OutEdgeIt;
-typedef graph_traits < Graph >::vertex_descriptor Vertex;
+typedef adjacency_list <vecS, vecS, undirectedS> Graph;
+typedef graph_traits<Graph>::vertex_descriptor Vertex;
+typedef graph_traits<Graph>::vertices_size_type VertexIndex;
+typedef VertexIndex* Rank;
+typedef Vertex* Parent;
 
-const I INF = std::numeric_limits<I>::max();
+/*
+typedef adjacency_list < vecS, vecS, undirectedS,
+  no_property, no_property > Graph;
+typedef graph_traits < Graph >::edge_descriptor Edge;
+typedef graph_traits < Graph >::edge_iterator EdgeIt;
+typedef graph_traits < Graph >::vertex_descriptor Vertex;
+*/
+
+const ll INF = std::numeric_limits<ll>::max();
 
 void testcase()
 {
   // read number of points
   int n,m;
-  I p;
+  long p;
   std::cin >> n >> m >> p;
   // read points
   std::vector<K::Point_2> pts;
@@ -50,92 +61,116 @@ void testcase()
     v->info() = i++;
   }
 
-  // Spanning tree on triangulation
-  Graph G1(n);
-  property_map < Graph, edge_weight_t >::type weight1 = get(edge_weight, G1);
-  for(auto v = t.finite_vertices_begin();v!=t.finite_vertices_end();++v){
-    auto c = t.incident_vertices(v);
-    if(c==0) continue;
-    do{
-      if(t.is_infinite(c)) continue;
-      Edge e; bool success;
-      tie(e,success) = add_edge(v->info(),c->info(),G1);
-      weight1[e] = CGAL::squared_distance(v->point(),c->point());
-    } while (++c != t.incident_vertices(v));
-  }
-
-  std::vector < Edge > spanning_tree;
-  kruskal_minimum_spanning_tree(G1, std::back_inserter(spanning_tree));
-
-  Graph G2(n);
-  property_map < Graph, edge_weight_t >::type weight2 = get(edge_weight, G2);
-  for(Edge e_ : spanning_tree){
-    Edge e; bool success;
-    tie(e,success) = add_edge(source(e_,G1),target(e_,G1),G2);
-    weight2[e] = weight1[e_];
-  }
-
-  I a = 0;
-  I b = 0;
+  std::vector<std::pair<int,int>> mis(m);
+  std::vector<ll> mis_d(m);
   for(int i=0;i<m;++i){
-    K::Point_2 p0,p1;
+    K::Point_2 p0, p1;
     std::cin >> p0 >> p1;
-
     Vertex_handle v0 = t.nearest_vertex(p0);
     Vertex_handle v1 = t.nearest_vertex(p1);
-    I g2 = std::max(CGAL::squared_distance(p0,v0->point()),
-                    CGAL::squared_distance(p1,v1->point()));
+    mis[i].first = v0->info();
+    mis[i].second = v1->info();
+    mis_d[i] = std::max(
+          CGAL::squared_distance(p0,v0->point()),
+          CGAL::squared_distance(p1,v1->point()));
+  }
 
-    int start = v0->info();
-    int end = v1->info();
 
-    std::vector<bool> visited(n,false);
-    std::vector<I>  best(n,INF);
-    std::vector<Vertex>  pred(n,-1);
-    best[v1->info()] = 0;
-    std::stack<int> Q;
-    Q.push(start);
-    bool keep_looking = true;
-    while(!Q.empty()){
-      Vertex v = Q.top();
-      if(visited[v]){
-        I b = best[v];
-        OutEdgeIt ebeg,eend;
-        for(tie(ebeg,eend) = out_edges(v,G2); ebeg!=eend;++ebeg){
-          Vertex u = target(*ebeg,G2);
-          if(pred[v]==u) continue;
-          I bi = std::max(best[u],weight2[*ebeg]);
-          if(bi < b) b = bi;
-        }
+  // <dist, <u,v>>
+  std::vector<std::pair<ll,std::pair<int,int>>> edges;
 
-        best[v] = b;
-        Q.pop();
-      } else {
-        visited[v] = true;
-        OutEdgeIt ebeg,eend;
-        for(tie(ebeg,eend) = out_edges(v,G2); ebeg!=eend;++ebeg){
-          Vertex u = target(*ebeg,G2);
-          if(!visited[u]) {
-            Q.push(u);
-            pred[u] = v;
-          }
-        }
-      }
+  for(Triangulation::Finite_edges_iterator it = t.finite_edges_begin();
+      it != t.finite_edges_end(); ++it){
+    ll dist = t.segment(*it).squared_length();
+    Vertex_handle v0 = it->first->vertex((it->second + 1) % 3);
+    Vertex_handle v1 = it->first->vertex((it->second + 2) % 3);
+    int u = std::min(v0->info(),v1->info());
+    int v = std::max(v0->info(),v1->info());
+    edges.push_back(std::make_pair(dist,
+                      std::make_pair(u,v)));
+  }
+  
+  std::sort(edges.begin(),edges.end());
+
+  // compute a
+  Graph G(n);
+  std::vector<VertexIndex> rank(n);
+  std::vector<Vertex> parent(n);
+
+  disjoint_sets<Rank, Parent> ds(&rank[0], &parent[0]);
+  initialize_incremental_components(G, ds);
+
+  int l=0;
+  ll a=0;
+  for(int i=0;i<m;++i){
+    while(ds.find_set(mis[i].first) != ds.find_set(mis[i].second)){
+      a = std::max(a,edges[l].first);
+      int u = edges[l].second.first;
+      int v = edges[l].second.second;
+      add_edge(u,v,G);
+      ds.union_set(u,v);
+      ++l;
     }
+    a = std::max(a,mis_d[i]*4);
+  }
 
-    g2 = std::max(4*g2,best[v0->info()]);
-    a = std::max(a,g2);
-    if(g2 <= p){
-      b = std::max(b,g2);
-      std::cout << 'y';
-    } else {
-      std::cout << 'n';
-    }
+  // compute b
+  G.clear();
+  G = Graph(n);
+  std::vector<bool> in(m);
+  rank.clear();
+  rank.resize(n);
+  parent.clear();
+  parent.resize(n);
+  ds = disjoint_sets<Rank,Parent>(&rank[0],&parent[0]);
+  initialize_incremental_components(G, ds);
+  for(auto e : edges){
+    if(e.first > p) break;
+    int u = e.second.first;
+    int v = e.second.second;
+    add_edge(u,v,G);
+    ds.union_set(u,v);
+  }
+
+  for(int i=0;i<m;++i){
+    in[i] = ds.find_set(mis[i].first) == ds.find_set(mis[i].second) &&
+            mis_d[i]*4<=p;
+    std::cout << (in[i] ? 'y' : 'n');
   }
   std::cout << std::endl;
+
+  G.clear();
+  G = Graph(n);
+  rank.clear();
+  rank.resize(n);
+  parent.clear();
+  parent.resize(n);
+
+  ds = disjoint_sets<Rank, Parent>(&rank[0], &parent[0]);
+  initialize_incremental_components(G, ds);
+
+  l=0;
+  ll b=0;
+  for(int i=0;i<m;++i){
+    if(!in[i]) continue;
+    while(ds.find_set(mis[i].first) != ds.find_set(mis[i].second)){
+      b = std::max(b,edges[l].first);
+      int u = edges[l].second.first;
+      int v = edges[l].second.second;
+      add_edge(u,v,G);
+      ds.union_set(u,v);
+      ++l;
+    }
+    b = std::max(b,mis_d[i]*4);
+  }
+
   std::cout << a << std::endl;
   std::cout << b << std::endl;
-}
+
+
+  
+
+  }
 
 int main(){
   std::ios_base::sync_with_stdio(false);
