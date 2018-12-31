5961ac2f466e59f4c4610235b9ec1d625ff749d0
Sandro Panighetti <sandro.panighetti@gmail.com>
2018-12-31 11:39:30 +0100

    updated planks

--- week11/planks/planks.cpp
+++ week11/planks/planks.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <numeric>
 #include <algorithm>
 #include <vector>
 #include <utility>
 #include <tuple>
-#include <set>
+#include <bitset>
 
 using namespace std;
+
+#define trace(x) //cerr << #x << " = " << x << endl
+#define btrace(x) //cerr << #x << " = " << bitset<10>(x) << endl
 
 typedef tuple<long,long,long,long> Four;
 
@@ -19,73 +23,45 @@ void testcase(){
   for(int i=0;i<n1;++i) cin >> L1[i];
   for(int i=0;i<n2;++i) cin >> L2[i];
 
-  long suml1 = 0;
-  for(int i=0;i<n1;++i) suml1 += L1[i];
-  long suml2 = 0;
-  for(int i=0;i<n2;++i) suml2 += L2[i];
-  if((suml1+suml2)%4 != 0) {
+  long sum_l = accumulate(L1.begin(),L1.end(),(long)0);
+  sum_l += accumulate(L2.begin(),L2.end(),(long)0);
+
+  if(sum_l%4 != 0) {
     cout << "0" << endl;
     return;
   }
-  long goal = (suml1+suml2)/4;
+  long goal = sum_l/4;
 
-  vector<Four> m_L1;
-  //vector<Four> m_L2;
-  set<Four> m_L2;
-
-  for(int i=0;i<(1<<n2*2);++i){
-    long a=0;
-    long b=0;
-    long c=0;
-    long d=0;
+  vector<Four> m_L2;
+  for(long i=0;i<(1<<n2*2);++i){
+    vector<long> u(4,0);
 
     for(int j=0;j<n2;++j){
-      int idx = (i & (3<<2*j)) >> 2*j;
-      if(idx==0) a += L2[j];
-      else if(idx==1) b += L2[j];
-      else if(idx==2) c += L2[j];
-      else if(idx==3) d += L2[j];
+      long idx = (i & (3<<2*j)) >> 2*j;
+      u[idx] += L2[j];
     }
 
-    if(a<=goal && b<=goal && c<= goal && d<=goal){
-      //m_L2.push_back(make_tuple(a,b,c,d));
-      m_L2.insert(make_tuple(a,b,c,d));
+    if(u[0]<=goal && u[1]<=goal && u[2]<= goal && u[3]<=goal){
+      m_L2.push_back(make_tuple(u[0],u[1],u[2],u[3]));
     }
   }
 
+  sort(m_L2.begin(),m_L2.end());
 
-  //sort(m_L2.begin(),m_L2.end());
-
-  for(int i=0;i<(1<<n1*2);++i){
-    long a=0;
-    long b=0;
-    long c=0;
-    long d=0;
+  long sol=0;
+  for(long i=0;i<(1<<n1*2);++i){
+    vector<long> u(4,0);
 
     for(int j=0;j<n1;++j){
-      int idx = (i & (3<<2*j)) >> 2*j;
-      if(idx==0) a += L1[j];
-      else if(idx==1) b += L1[j];
-      else if(idx==2) c += L1[j];
-      else if(idx==3) d += L1[j];
+      long idx = (i & (3<<2*j)) >> 2*j;
+      u[idx] += L1[j];
     }
     
-    Four x = make_tuple(goal-a,goal-b,goal-c,goal-d);
-    if(m_L2.count(x)>0){
-      cout << 1 << endl;
-      return;
-    }
+    Four x = make_tuple(goal-u[0],goal-u[1],goal-u[2],goal-u[3]);
+    auto bounds = equal_range(m_L2.begin(),m_L2.end(),x);
+    sol += distance(bounds.first,bounds.second);
   }
-
-  /*
-  for(auto x : m_L2){
-    cout << get<0>(x) << " " << get<1>(x) << " " << get<2>(x) << " " << get<3>(x) << endl;
-  }
-  */
-
-  cout << 0 << endl;
-
-
+  cout << sol/(long)24 << endl;
 }
 
 int main(){
