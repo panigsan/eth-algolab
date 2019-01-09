f2c68dcee463d5e97962010f89effa99057b534b
Sandro Panighetti <sandro.panighetti@gmail.com>
2019-01-09 18:35:22 +0100

    light pattern 100

--- week12/light_pattern/light.cpp
+++ week12/light_pattern/light.cpp
@@ -10,28 +10,38 @@ using namespace std;
 #define btrace(x) cerr << #x << " = " << bitset<2>(x) << endl
 #define one bitset<B>(1)
 
+int n,k,x;
 bitset<B> l;
+bitset<B> bx;
 vector<int> dp;
 long solve(int i, bool inv){
   if(i==-1) return 0;
   long idx = (i<<1) | inv;
   if(dp[idx]!=-1) return dp[idx];
 
-  long best;
-  // if no need to invert, then goes to next one
-  if(inv==l.test(i)) best = solve(i-1,inv);
-  // otherwise try the two options
-  else best = min(1+solve(i-1,inv),1+solve(i-1,!inv));
+  // number of changes to the pattern
+  long changes_to_norm = 0;
+  for(int j=0;j<k;++j){
+    // keeping track if the patter is flipped or not
+    if(inv){
+      changes_to_norm += bx.test(j) == l.test(i*k+k-j-1);
+    } else {
+      changes_to_norm += bx.test(j) != l.test(i*k+k-j-1);
+    }
+  }
+  // either change the single bits of the current row OR
+  // change them in the opposite way and flip the whole rows
+  long best = min(changes_to_norm+solve(i-1,inv),(k-changes_to_norm) + 1 +solve(i-1,!inv));
 
   dp[idx] = best;
   return best;
 }
 
 void testcase(){
-  int n,k,x;
   cin >> n >> k >> x;
+  bx = bitset<B>(x);
   l = 0;
-  for(int i=1;i<=n;++i){
+  for(int i=0;i<n;++i){
     bool b;
     cin >> b;
     l.set(i,b);
@@ -40,7 +50,7 @@ void testcase(){
   dp.clear();
   dp.resize(n*2,-1);
 
-  long cnt = solve(n-1,false);
+  long cnt = solve(n/k-1,false);
   cout << cnt << endl;
 
 }
