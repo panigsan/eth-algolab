aac6544190c50c0ba8b43e685a0eee34766ec5f7
Sandro Panighetti <sandro.panighetti@gmail.com>
2019-01-11 11:46:00 +0100

    casino royale 100

---
+++ week12/casino_royale/casino.cpp
@@ -0,0 +1,101 @@
+// ALGOLAB BGL Tutorial 3
+// Code snippets demonstrating 
+// - MinCostMaxFlow with arbitrary edge costs using cycle_canceling
+// - MinCostMaxFlow with non-negative edge costs using successive_shortest_path_nonnegative_weights
+
+// Compile and run with one of the following:
+// g++ -std=c++11 -O2 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+// g++ -std=c++11 -O2 -I path/to/boost_1_58_0 bgl_mincostmaxflow.cpp -o bgl_mincostmaxflow; ./bgl_mincostmaxflow
+
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <cstdlib>
+// BGL includes
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/cycle_canceling.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+#include <boost/graph/successive_shortest_path_nonnegative_weights.hpp>
+#include <boost/graph/find_flow_cost.hpp>
+
+// BGL Graph definitions
+// ===================== 
+// Graph Type with nested interior edge properties for Cost Flow Algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> Traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, Traits::edge_descriptor,
+                boost::property <boost::edge_weight_t, long> > > > > Graph; // new!
+// Interior Property Maps
+typedef boost::property_map<Graph, boost::edge_capacity_t>::type      EdgeCapacityMap;
+typedef boost::property_map<Graph, boost::edge_weight_t >::type       EdgeWeightMap; // new!
+typedef boost::property_map<Graph, boost::edge_residual_capacity_t>::type ResidualCapacityMap;
+typedef boost::property_map<Graph, boost::edge_reverse_t>::type       ReverseEdgeMap;
+typedef boost::graph_traits<Graph>::vertex_descriptor          Vertex;
+typedef boost::graph_traits<Graph>::edge_descriptor            Edge;
+typedef boost::graph_traits<Graph>::out_edge_iterator  OutEdgeIt; // Iterator
+
+// Custom Edge Adder Class, that holds the references
+// to the graph, capacity map, weight map and reverse edge map
+// ===============================================================
+class EdgeAdder {
+    Graph &G;
+    EdgeCapacityMap &capacitymap;
+    EdgeWeightMap &weightmap;
+    ReverseEdgeMap  &revedgemap;
+
+public:
+    EdgeAdder(Graph &G, EdgeCapacityMap &capacitymap, EdgeWeightMap &weightmap, ReverseEdgeMap &revedgemap) 
+        : G(G), capacitymap(capacitymap), weightmap(weightmap), revedgemap(revedgemap) {}
+
+    void addEdge(int u, int v, long c, long w) {
+        Edge e, rev_e;
+        boost::tie(e, boost::tuples::ignore) = boost::add_edge(u, v, G);
+        boost::tie(rev_e, boost::tuples::ignore) = boost::add_edge(v, u, G);
+        capacitymap[e] = c;
+        weightmap[e] = w; // new!
+        capacitymap[rev_e] = 0;
+        weightmap[rev_e] = -w; // new
+        revedgemap[e] = rev_e; 
+        revedgemap[rev_e] = e; 
+    }
+};
+
+void testcase() {
+  int n,m,l;
+  std::cin >> n >> m >> l;
+ 
+  // Create Graph and Maps
+  Graph G(n);
+  EdgeCapacityMap capacitymap = boost::get(boost::edge_capacity, G);
+  EdgeWeightMap weightmap = boost::get(boost::edge_weight, G);
+  ReverseEdgeMap revedgemap = boost::get(boost::edge_reverse, G);
+  EdgeAdder eaG(G, capacitymap, weightmap, revedgemap);
+
+  Vertex src = add_vertex(G);
+
+  for(int i=0;i<m;++i){
+    int x,y,q;
+    std::cin >> x >> y >> q;
+    eaG.addEdge(x,y,1,(256-q)+256*(y-x-1));
+  }
+
+  for(int i=0;i<n-1;++i){
+    eaG.addEdge(i,i+1,10000,256);
+  }
+
+  eaG.addEdge(src,0,l,0);
+  
+  boost::successive_shortest_path_nonnegative_weights(G, src, n-1);
+  int cost2 = -boost::find_flow_cost(G) + 256*(n-1)*l;
+  std::cout << cost2 << std::endl;
+}
+
+int main(){
+  std::ios_base::sync_with_stdio(false);
+  int T;
+  std::cin >> T;
+  while(T--) testcase();
+}
