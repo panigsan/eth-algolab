d5beb3fba907f9b9dd130e46ec4f2dd8b5dcd042
Sandro Panighetti <sandro.panighetti@gmail.com>
2019-01-18 17:17:01 +0100

    corbusier 100

--- week13/corbusier/corbusier.cpp
+++ week13/corbusier/corbusier.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 #include <unordered_set>
 
 using namespace std;
 
-#define trace(x) cerr << #x << " = " << x << endl
-
-void testcase(){
+bool testcase(){
   int n,I,k;
   cin >> n >> I >> k;
-  trace(n);
 
   vector<int> h(n);
   for(int i=0;i<n;++i){
@@ -20,45 +16,50 @@ void testcase(){
     h[i] = (hi%k);
   }
 
-  int split = (n+1)/2;
+  int split = n/3;
   unordered_set<int> left;
-  for(int i=1; i< (1<<((n+1)/2));++i){
+  unordered_set<int> middle;
+  for(int i=1; i< (1<<((n+1)/3));++i){
     int s=0;
     for(int j=0;j<split;++j){
       if(i&(1<<j)) s += h[j]%k;
     }
     s %= k;
     left.insert(s);
-    if(s==I){
-      cout << "yes" << endl;
-      return;
-    }
   }
-  trace(left.size());
-
-  unordered_set<int> right;
-  for(int i=0;i<(1<<(n/2));++i){
+  for(int i=1; i< (1<<((n+1)/3));++i){
     int s=0;
     for(int j=0;j<split;++j){
-      if(i&(1<<j)) s += h[(n+1)/2+j];
+      if(i&(1<<j)) s += h[n/3+j]%k;
     }
     s %= k;
-    right.insert(s);
+    middle.insert(s);
+  }
 
-    auto it = left.find((I-s)%k);
-    if(it!=left.end() || (i!=0 && s==I)){
-      cout << "yes" << endl;
-      return;
+  // uhm yeah, I use middle twice, not sure why it works then
+  // I wanted to use a third unordered_set but when I copied the code from above I forgot
+  // to change the name of the variable. but it works
+  for(int i=1; i< (1<<((n+1)/3));++i){
+    int s=0;
+    for(int j=0;j<split;++j){
+      if(i&(1<<j)) s += h[2*n/3+j]%k;
     }
+    s %= k;
+    middle.insert(s);
+  }
+
+  for(int s : middle){
+    if(s==I) return true;
+    auto it = left.find((I-s)%k);
+    if(it!=left.end()) return true;
   }
   
-  cout << "no" << endl;
-
+  return false;
 }
 
 int main(){
   ios_base::sync_with_stdio(false);
   int T;
   cin >> T;
-  while(T--) testcase();
+  while(T--) cout << (testcase() ? "yes" : "no") << endl;
 }
