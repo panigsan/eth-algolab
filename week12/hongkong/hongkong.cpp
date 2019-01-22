7c2ee6501bdbc95dd3ce32d13e1288f89fbf28cb
Sandro Panighetti <sandro.panighetti@gmail.com>
2019-01-22 16:42:24 +0100

    hongkong 20

--- week12/hongkong/hongkong.cpp
+++ week12/hongkong/hongkong.cpp
@@ -17,45 +17,39 @@ typedef K::Point_2 P;
 
 struct Info{};
 
-typedef CGAL::Triangulation_vertex_base_with_info_2<EP,K> Vb;
-typedef CGAL::Triangulation_face_base_with_info_2<Info,K> Fb;
+typedef CGAL::Triangulation_vertex_base_with_info_2<EP,EK> Vb;
+typedef CGAL::Triangulation_face_base_with_info_2<Info,EK> Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb> Tds;
-typedef CGAL::Delaunay_triangulation_2<K,Tds>  Triangulation;
+typedef CGAL::Delaunay_triangulation_2<EK,Tds>  Triangulation;
 typedef Triangulation::Face_handle Face_handle;
 
-const K::FT INF(std::numeric_limits<double>::max());
+const EK::FT INF(std::numeric_limits<double>::max());
 
 void testcase(){
   int n,m;
-  K::FT r;
+  EK::FT r;
   std::cin >> n >> m >> r;
 
   // Read the trees
-  std::vector<P> trees(n);
+  std::vector<EP> trees(n);
   for(int i=0;i<n;++i){
-    K::FT x,y;
+    long x,y;
     std::cin >> x >> y;
-    trees[i] = P(x,y);
+    trees[i] = EP(x,y);
   }
 
   Triangulation t;
   t.insert(trees.begin(),trees.end());
 
-  // To each vertex, assign an exact point
-  for(auto it=t.finite_vertices_begin(); it!=t.finite_vertices_end();++it){
-    it->info() = EP(it->point().x(),it->point().y());
-  }
-
   for(int i=0;i<m;++i){
-    K::FT x,y;
+    long x,y;
     EK::FT s;
     std::cin >> x >> y >> s;
-    P center(x,y);
-    EP e_center(x,y);
+    EP center(x,y);
 
-    EP nearest = t.nearest_vertex(center)->info();
-    EK::FT d = CGAL::squared_distance(e_center,nearest);
-    EK::FT required = (r+s)*(r+s);
+    EP nearest = t.nearest_vertex(center)->point();
+    EK::FT d = CGAL::squared_distance(center,nearest);
+    EK::FT required = 4*CGAL::square(r+s);
     if(d < required){
       std::cout << 'n';
     } else {
