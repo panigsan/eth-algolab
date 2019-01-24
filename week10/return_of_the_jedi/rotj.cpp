afa18a9baa714a4d904033b3f282ff5aa29ea678
Sandro Panighetti <sandro.panighetti@gmail.com>
2019-01-24 15:32:41 +0100

    return of the jedi 100

--- week10/return_of_the_jedi/rotj.cpp
+++ week10/return_of_the_jedi/rotj.cpp
@@ -1,87 +1,95 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <utility>
+#include <stack>
 #include <algorithm>
-#include <tuple>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/prim_minimum_spanning_tree.hpp>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include <boost/graph/connected_components.hpp>
+#include <boost/graph/max_cardinality_matching.hpp>
+
+
+// BGL Graph definitions
+// =====================
+// Graph Type, OutEdgeList Type, VertexList Type, (un)directedS
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,   
+  boost::no_property,
+    boost::property<boost::edge_weight_t, long>
+    > Graph;
+typedef boost::graph_traits<Graph>::edge_descriptor   Edge;
+typedef boost::graph_traits<Graph>::vertex_descriptor   Vertex;
+typedef boost::graph_traits<Graph>::edge_iterator   EdgeIt;
+typedef boost::graph_traits<Graph>::out_edge_iterator   OutEdgeIt;
+typedef boost::property_map<Graph, boost::edge_weight_t>::type  WeightMap;
 
 using namespace std;
 
-int n,i;
-vector<vector<int>> edges;
-// run algorithm avoiding the edge between no1 and no2
-int algo(vector<vector<int>>& new_edges,int start, int no1, int no2){
-  vector<bool> visited(n+1,false);
-  priority_queue<tuple<int,int,int>> Q;
-  visited[start] = true;
-  for(int j=1;j<=n;++j) Q.push(make_tuple(-edges[start][j],j,start));
+void testcase(){
+  int n,i;
+  cin >> n >> i;
+  
+  Graph G(n);
+  WeightMap weightmap = get(boost::edge_weight, G);
+  for(int j=0;j<n-1;++j){
+    for(int k=j+1;k<n;++k){
+      long c;
+      cin >> c;
+      Edge e; bool success;
+      tie(e,success) = boost::add_edge(j,k,G);
+      weightmap[e]=c;
+    }
+  }
 
-  int tot_cost = 0;
-  int added=1;
-  while(added < n){
-    auto e = Q.top(); Q.pop(); 
-    int cost = -get<0>(e);
-    int u = get<1>(e);
-    int v = get<2>(e);
-    if((u==no1 && v==no2) || (u==no2 && v==no1)) continue;
-    if(visited[u]) continue;
+  std::vector<Vertex> primpredmap(n);
+  boost::prim_minimum_spanning_tree(G, boost::make_iterator_property_map(primpredmap.begin(), boost::get(boost::vertex_index, G)));
+  
+  Graph mst(n);
+  WeightMap weightmap_mst = get(boost::edge_weight, mst);
+  long tot=0;
+  for(int j=0;j<n;++j){
+    if(primpredmap[j]!=j){
+      Edge e; bool success;
+      tie(e,success) = boost::edge(j,primpredmap[j],G);
+      long cost = weightmap[e];
+      weightmap[e]=-1; // store that this edge belongs to mst
+      tie(e,success) = boost::add_edge(j,primpredmap[j],mst);
+      weightmap_mst[e]=cost;
+      tot += cost;
+    } 
+  }
 
-    new_edges[u].push_back(v);
-    new_edges[v].push_back(u);
-    tot_cost += cost;
-    visited[u] = true;
-    for(int j=1;j<=n;++j) Q.push(make_tuple(-edges[u][j],j,u));
-
-    ++added;
+  std::vector<std::vector<long>> DP(n,std::vector<long>(n,0));
+  for(int j=0;j<n;++j){
+    std::queue<int> Q;
+    std::vector<bool> visited(n,false);
+    Q.push(j);
+    while(!Q.empty()){
+      int next = Q.front(); Q.pop();
+      visited[next] = true;
+      OutEdgeIt ebeg, eend;
+      for(boost::tie(ebeg,eend) = boost::out_edges(next,mst);ebeg!=eend;++ebeg){
+        Vertex v = boost::target(*ebeg,mst);
+        if(!visited[v]){
+          Q.push(v);
+          DP[j][v] = std::max(DP[j][next], weightmap_mst[*ebeg]);
+        }
+      }
+    }
   }
   
-  return tot_cost;
-}
-int algo(vector<vector<int>>& new_edges,int start){
-  return algo(new_edges,start,-1,-1);
-}
-
-void testcase(){
-  cin >> n >> i;
-
-  edges.clear();
-  edges.resize(n+1,vector<int>(n+1,-1));
-  vector<vector<bool>> b_edges(n+1,vector<bool>(n+1,false));
-  for(int j=1;j<=n-1;++j){
-    for(int k=1;k<=n-j;++k){
-      int c;
-      cin >> c;
-      edges[j][j+k] = c;
-      edges[j+k][j] = c;
+  long best_diff=std::numeric_limits<long>::max();
+  EdgeIt ebeg,eend;
+  for(tie(ebeg,eend) = boost::edges(G);ebeg!=eend;++ebeg){
+    if(weightmap[*ebeg]!=-1){
+      long diff = weightmap[*ebeg]-DP[boost::target(*ebeg,G)][boost::source(*ebeg,G)];
+      best_diff = std::min(best_diff,diff);
     }
   }
 
-  vector<vector<int>> new_edges(n+1,vector<int>());
-  int tot_cost = algo(new_edges,i);
+  std::cout << tot + best_diff << std::endl;
 
-  int best = numeric_limits<int>::max();
-
-  if(n>100){ // testcase 2
-    for(int j=1;j<=n;++j){
-      if(new_edges[j].size()!=1) continue;
-
-      for(int k=1;k<=n;++k){
-        if(k == j || k == new_edges[j][0]) continue;
-        int current = tot_cost - edges[j][new_edges[j][0]] + edges[j][k]; 
-        best = min(best,current);
-      }
-    }
-
-  } else { // testcase 1
-    for(int j=1;j<=n;j++){
-      for(int k=0;k<new_edges[j].size();++k){
-        vector<vector<int>> new_edges2(n+1,vector<int>());
-        int current = algo(new_edges2,j,j,new_edges[j][k]);
-        best = min(best,current);
-      }
-    }
-  }
-  cout << best << endl;
 }
 
 int main(){
